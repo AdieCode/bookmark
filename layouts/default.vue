106c4da0fb7381f6005d59dee3825ff222cf3474
AdieCode <Adiecode4@gmail.com>
2024-09-22 18:10:26 +0200

    updating the responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="flex flex-col w-screen">
-        <div class="flex flex-row place-content-center justify-between m-4">
+    <div class="flex flex-col">
+        <div class="flex flex-row place-content-center justify-between mt-4 mb-4">
             <div class="w-40 cursor-pointer">
                 <img src="../public/images/Logo-bookmark.png" alt="">
             </div>
-            <!-- your nav bar -->
-                <div class="w-80 h-12 border-4 border-black rounded-xl m-auto cursor-pointer flex items-center text-black/40 font-medium hover:w-96 hover:text-black/80 hover:font-bold duration-100 ">
+
+            <!-- Desktop/Tablet Search Bar -->
+            <div class="hidden md:flex w-80 h-12 border-4 border-black rounded-xl m-auto cursor-pointer flex items-center text-black/40 font-medium hover:w-96 hover:text-black/80 hover:font-bold duration-100">
                 <span class="ml-3">Search for anime/manga here</span>
                 <div class="h-full w-14 border-l-4 border-black ml-auto flex justify-center items-center">
                     <img src="../public/images/search.png" alt="" class="w-7 h-7">
                 </div>
             </div>
-    
+
+            <!-- Mobile Search Icon -->
+            <div class="md:hidden w-14 h-12 border-4 border-black rounded-xl flex justify-center items-center ml-auto cursor-pointer">
+                <img src="../public/images/search.png" alt="Search Icon" class="w-7 h-7">
+            </div>
     
             <div class="w-20 cursor-pointer">
                 <img src="../public/images/User.png" alt="">
             </div>
         </div>
 
-        <div class="flex flex-row place-content-center justify-around mt-1">
-            <div class="flex flex-row gap-4">
+        <div class="flex flex-row items-center justify-around mt-1 mx-2">
+            <div class="flex flex-row flex-wrap gap-4">
                 <div class="p-2 border-4 border-black rounded-xl m-auto cursor-pointer flex items-center justify-around text-xl font-bold hover:bg-black hover:text-white transition-colors duration-100">
                     Discover
                 </div>
@@ -34,10 +39,9 @@
                 </div>
             </div>
         </div>
-        <div class="fixed bottom-10 right-10 w-16 h-16 border-4 border-black rounded-2xl cursor-pointer flex justify-center items-center hover:shadow-lg hover:bottom-11 duration-100">
+        <div class="fixed bottom-10 right-10 w-16 h-16 border-4 bg-white border-black rounded-2xl cursor-pointer flex justify-center items-center hover:shadow-lg hover:bottom-11 duration-100 z-10">
             <img src="../public/images/plus.png" alt="" class="w-8 h-8">
         </div>
-
 
     </div>
     <div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-3/4 mt-10 mx-auto flex flex-row justify-center flex-wrap gap-6">
+    <div class="w-3/4 mt-10 mx-auto flex flex-row justify-center flex-wrap gap-8 sm:w-full">
         <ContentCard/>
         <ContentCard/>
         <ContentCard/>
