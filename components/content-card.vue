335014fb65024db7d38f6ac479e7c12cfccbf89e
AdieCode <adiecode4@gmail.com>
2025-06-18 17:26:58 +0200

    forgot to remove

--- components/content-card.vue
+++ components/content-card.vue
@@ -7,7 +7,7 @@
             
             <!-- Info section, visible when showInfo is true -->
             <div v-if="showInfo" class="absolute w-full z-10 p-2 bottom-4 flex justify-center items-center gap-2 bounce-down">
-                <div class="p-3 bg-white border-2 border-black rounded-lg cursor-pointer" @click="moreInfo">
+                <div class="p-3 bg-white border-2 border-black rounded-lg cursor-pointer">
                     <a :href="'/info?id=' + data.anilist_content_id" title="Go to homepage">
                         <span class="font-bold text-base pb-4">More info</span>
                     </a>
