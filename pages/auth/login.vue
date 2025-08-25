5966cdb20d3cd65b6c7445362a5ebffa60164ca4
AdieCode <adiecode4@gmail.com>
2025-08-25 22:59:42 +0200

    github redirect url update

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -83,7 +83,7 @@ const email = ref('');
 const password = ref('');
 
 const googleOAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=1092384774274-2s5jb68iobabko0rk1rumcbnbt5laf26.apps.googleusercontent.com&redirect_uri=${config.public.baseUrl}/auth/google/callback&response_type=code&scope=email%20profile`;
-const githubOAuthUrl = "https://github.com/login?client_id=Ov23liwFfm4hn8BaO6bS&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3DOv23liwFfm4hn8BaO6bS%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A3001%252Fauth%252Fgithub%252Fcallback";
+const githubOAuthUrl = `https://github.com/login/oauth/authorize?client_id=Ov23liwFfm4hn8BaO6bS&redirect_uri=${config.public.baseUrl}/auth/github/callback`;
 
 function redirectToGoogle() {
     useToggles.setNotification('Google login', 0);
